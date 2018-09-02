a79d69c40b05da4a6b9ab0b314d6d0cb27d59b0f
Michael Zech <michael.zech@hotmail.de>
2018-09-02 16:26:34 +0200

    Improved the project-structure.

--- assignment01/src/App.js
+++ assignment01/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import UserInput from "./UserInput";
-import UserOutput from "./UserOutput";
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
 import "./App.css";
 
 class App extends Component {
