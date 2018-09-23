865faf490e5e0d589d2ab473eb941277408d083d
Michael Zech <michael.zech@hotmail.de>
2018-09-23 15:19:10 +0200

    Added the BuildControl components.

--- burger-builder-app/src/components/Burger/Burger.js
+++ burger-builder-app/src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const Burger = props => {
-  const transformedIngredient = Object.keys(props.ingredients).map(key => {
-    return [...Array(props.ingredients[key])].map((_, i) => {
-      return <BurgerIngredient key={key + i} type={key} />;
-    });
-  });
+  let transformedIngredient = Object.keys(props.ingredients)
+    .map(key => {
+      return [...Array(props.ingredients[key])].map((_, i) => {
+        return <BurgerIngredient key={key + i} type={key} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredient.length === 0) {
+    transformedIngredient = <p>Please start to add ingredients.</p>;
+  }
 
   return (
     <div className={classes.Burger}>
