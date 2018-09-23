ebe66e84137015e75b2a694cd361e60e8406a839
Michael Zech <michael.zech@hotmail.de>
2018-09-23 14:23:13 +0200

    First steps done to make the app dynamic.

--- burger-builder-app/src/components/Burger/Burger.js
+++ burger-builder-app/src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const Burger = props => {
+  const transformedIngredient = Object.keys(props.ingredients).map(key => {
+    return [...Array(props.ingredients[key])].map((_, i) => {
+      return <BurgerIngredient key={key + i} type={key} />;
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredient}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,21 @@ import Aux from "../../hocs/Aux";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      BreadTop: 1,
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+      BreadBottom: 1
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Builder</div>
       </Aux>
     );
