daafca3b5e7a71edd3a3b85cdd86cea88b85abaa
Michael Zech <michael.zech@hotmail.de>
2018-09-24 18:33:33 +0200

    Continued with enhancing BuildControl.

--- burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 import classes from "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
 
-const BuildControls = (props) => (
-    return ();
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
+const BuildControls = props => (
+  <div className={classes.BuildControl}>
+    {controls.map(ctrl => (
+      <BuildControl
+        added={props.ingredientAdded}
+        key={ctrl.label}
+        label={ctrl.label}
+      />
+    ))}
+  </div>
 );
 
 export default BuildControls;
